06e66a10d595ecd766e5da63173804d8bf46d9f5
naeem-ahsan <naeemkb0167@gmail.com>
2022-05-19 11:56:15 +0800

    individual profile added

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { ChevronDownIcon, ChevronRightIcon, ChevronLeftIcon, PencilIcon } from '@heroicons/react/solid';
 import { FaDiscord, FaTwitter } from 'react-icons/fa';
 import Cite from '../assets/Cite.png';
 import Fish1 from '../assets/image 331.png';
@@ -14,7 +14,9 @@ import ProfileIcon2 from '../assets/image 128.png';
 import dp from '../assets/image 328.png';
 import Avatar from '../assets/Avatar.png';
 import Avatar2 from '../assets/Avatar2.png';
-import Avatar3 from '../assets/Avatar3.png'
+import Avatar3 from '../assets/Avatar3.png';
+import img217 from '../assets/image 217.png';
+import infinix from '../assets/infinix.png';
 
 const Main = () => {
   const [dropDown, setdropDown] = useState(false)
@@ -160,7 +162,7 @@ const Main = () => {
         {/*--- tabs left side end ---*/}
 
         {/* tabs right side */}
-        <div className="lg:col-span-2 col-span-3 bg-bg-light border-border-color rounded-lg border-2 overflow-hidden h-[1160px]">
+        <div className="lg:col-span-2 col-span-3 bg-bg-light border-border-color rounded-lg border-2 overflow-hidden h-auto">
 
           {/* tab 1 */}
           <div className={openTab === 1 ? "block px-6 py-8" : "hidden"} id="link1">
@@ -248,13 +250,23 @@ const Main = () => {
             {/* applicants content */}
             <div className="block md:flex justify-between px-6 py-8">
               <div className="flex">
-                <img className="rounded-xl w-32" src={dp} alt="/" />
+                <a
+                  href="/"
+                  onClick={e => {
+                    e.preventDefault();
+                    setOpenTab(3);
+                  }}
+                  data-toggle="tab"
+                  role="tablist"
+                >
+                  <img className="rounded-xl w-32 h-full" src={dp} alt="/" />
+                </a>
                 <div className=" text-white ml-8">
                   <span className=" bg-red-500 py-1 px-2 rounded-md text-xs">New</span><span className=" uppercase ml-2 text-sm font-extrabold text-yellow-400 font-bungee">Joshua dela cruz</span>
                   <p className="my-2">Philippines</p>
                   <div className="flex ">
-                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400"/>Roshanjill#1947</a>
-                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400"/>@leokras</a>
+                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
                   </div>
                   <div className=" mt-3 flex">
                     <span className="flex"><img className="h-[28px] w-[28px] rounded-xl mr-2" src={ProfileIcon1} alt="/" /> Guild: Yield Guild Game</span>
@@ -285,8 +297,8 @@ const Main = () => {
                   <span className=" bg-red-500 py-1 px-2 rounded-md text-xs">New</span><span className=" uppercase ml-2 text-sm font-extrabold text-yellow-400 font-bungee">Gonzalo Joshua</span>
                   <p className="my-2">Philippines</p>
                   <div className="flex ">
-                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400"/>Roshanjill#1947</a>
-                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400"/>@leokras</a>
+                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
                   </div>
                   <div className=" mt-3 flex">
                     <span className="flex"><img className="h-[28px] w-[28px] rounded-xl mr-2" src={ProfileIcon1} alt="/" /> Guild: Yield Guild Game</span>
@@ -317,8 +329,8 @@ const Main = () => {
                   <span className=" bg-red-500 py-1 px-2 rounded-md text-xs">New</span><span className=" uppercase ml-2 text-sm font-extrabold text-yellow-400 font-bungee">alysha degracia</span>
                   <p className="my-2">Philippines</p>
                   <div className="flex ">
-                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400"/>Roshanjill#1947</a>
-                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400"/>@leokras</a>
+                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
                   </div>
                   <div className=" mt-3 flex">
                     <span className="flex"><img className="h-[28px] w-[28px] rounded-xl mr-2" src={ProfileIcon1} alt="/" /> Guild: Yield Guild Game</span>
@@ -349,8 +361,8 @@ const Main = () => {
                   <span className=" bg-red-500 py-1 px-2 rounded-md text-xs">New</span><span className=" uppercase ml-2 text-sm font-extrabold text-yellow-400 font-bungee">angel bernila</span>
                   <p className="my-2">Philippines</p>
                   <div className="flex ">
-                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400"/>Roshanjill#1947</a>
-                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400"/>@leokras</a>
+                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
                   </div>
                   <div className=" mt-3 flex">
                     <span className="flex"><img className="h-[28px] w-[28px] rounded-xl mr-2" src={ProfileIcon1} alt="/" /> Guild: Yield Guild Game</span>
@@ -381,8 +393,8 @@ const Main = () => {
                   <span className=" bg-red-500 py-1 px-2 rounded-md text-xs">New</span><span className=" uppercase ml-2 text-sm font-extrabold text-yellow-400 font-bungee">joshua angelo</span>
                   <p className="my-2">Philippines</p>
                   <div className="flex ">
-                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400"/>Roshanjill#1947</a>
-                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400"/>@leokras</a>
+                    <a href="/" className=" bg-bg py-1 px-4 rounded-xl flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                    <a href="/" className="ml-3 bg-bg py-1 px-4 rounded-xl flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
                   </div>
                   <div className=" mt-3 flex">
                     <span className="flex"><img className="h-[28px] w-[28px] rounded-xl mr-2" src={ProfileIcon1} alt="/" /> Guild: Yield Guild Game</span>
@@ -407,6 +419,142 @@ const Main = () => {
 
           </div>
           {/*--- tab 2 end ---*/}
+
+          {/* inner tab 3 */}
+          <div className={openTab === 3 ? "block px-6 py-8" : "hidden"} id="link3">
+            {/* top content */}
+            <div className="block md:flex justify-between">
+              <ChevronLeftIcon className=" w-8 h-8 place-self-center text-white" />
+              <div className=" flex">
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 place-self-center text-white mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </button>
+                <button className="text-white py-2 px-4 border-2 border-border-color rounded-lg bg-gradient-to-r from-[#7A5834]  to-[#4A4039] h-11 place-self-center mt-4 md:mt-0 flex">
+                  Action
+                  <ChevronDownIcon className=" w-6 h-7 ml-3" />
+                </button>
+              </div>
+            </div>
+
+            <div className=" bg-[#282830] rounded-lg py-6  px-5 mt-12">
+              <div className=" grid grid-cols-6 gap-1">
+                <div className="col-span-1">
+                  <img className="rounded-xl w-32" src={dp} alt="/" />
+                </div>
+                <div className="flex col-span-5">
+                  <div className=" text-white ml-4">
+                    <div className="flex justify-between">
+                      <div className=" uppercase text-sm font-extrabold text-yellow-400 font-bungee">Gonzalo Joshua</div>
+                      <div className="flex">
+                        <button className="text-white py-[3px] px-3 border-2 border-border-color rounded-md bg-gradient-to-r from-[#7A5834]  to-[#4A4039] h-8  mt-4 md:mt-0 flex text-sm">
+                          Share Profile
+                        </button>
+                        <PencilIcon className=" w-7 h-7 ml-3 text-white hover:cursor-pointer" />
+                      </div>
+                    </div>
+                    <p>Philippines</p>
+                    <p className="my-2 text-sm">I am cool but not the coolest man you’ve seen before, cool as cool so cool the sun freezes in the midst of my presence cool </p>
+                    <div className="flex ">
+                      <a href="/" className=" py-1 px-4 flex"><FaDiscord className="place-self-center mr-1 text-blue-400" />Roshanjill#1947</a>
+                      <a href="/" className="ml-4 py-1 px-4 flex "><FaTwitter className="place-self-center mr-1 text-blue-400" />@leokras</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className=" mt-6 grid grid-cols-2 gap-1">
+                <span className=" col-span-1 flex justify-between border-2 border-border-color rounded-lg py-2 px-3">
+                  <p className="text-white">Guild: <br />Yield Guild Game</p>
+                  <img className="h-[46px] w-[46px] rounded-md" src={ProfileIcon1} alt="/" />
+                </span>
+                <span className="ml-4 col-span-1 flex justify-between border-2 border-border-color rounded-lg py-2 px-3">
+                  <p className="text-white">Top game: <br />Axie Infinity</p>
+                  <img className="h-[46px] w-[46px] rounded-md " src={ProfileIcon2} alt="" />
+                </span>
+              </div>
+            </div>
+            {/* top content end */}
+
+            {/* mid content */}
+            <div className=" my-6">
+              <h1 className=" uppercase text-white text-lg font-bungee">experience</h1>
+            </div>
+
+            <div className=" bg-[#282830] rounded-lg py-6  px-5">
+              <div className="flex">
+                <img src={Cite} alt="/" />
+                <div className=" text-white ml-4">
+                  <h1 className=" text-yellow-400 text-2xl mb-1 font-bungee">ABP CRIT BEAST</h1>
+                  <div>
+                    <span>Yield Guild Games</span>
+                    <span className="ml-3">&bull; Highest MMR: 2.823</span>
+                    <span className="ml-3"><span className="text-white">&bull; </span>July 2021 - Sept 2021</span>
+                  </div>
+                </div>
+              </div>
+              <div className=" px-0 lg:px-24 pt-4 lg:pt-0 ">
+                <p className="text-white pb-4">
+                  Was playing at YGG and then became a trainer due to my good performance. Was playing at
+                  YGG and then became a trainer due to my good performance. Was playing at YGG and then
+                  became a trainer due to my good performance.
+                </p>
+                <img className="pb-5" src={img217} alt="" />
+              </div>
+
+              <div className="flex mt-10">
+                <img src={Cite} alt="/" />
+                <div className=" text-white ml-4">
+                  <h1 className=" text-yellow-400 text-2xl mb-1 font-bungee">ABP CRIT BEAST</h1>
+                  <div>
+                    <span>Yield Guild Games</span>
+                    <span className="ml-3">&bull; Highest MMR: 2.823</span>
+                    <span className="ml-3"><span className="text-white">&bull; </span>July 2021 - Sept 2021</span>
+                  </div>
+                </div>
+              </div>
+              <div className=" px-0 lg:px-24 pt-4 lg:pt-0 ">
+                <p className="text-white pb-4">
+                  Was playing at YGG and then became a trainer due to my good performance. Was playing at
+                  YGG and then became a trainer due to my good performance. Was playing at YGG and then
+                  became a trainer due to my good performance.
+                </p>
+                <img src={img217} alt="" />
+              </div>
+            </div>
+            {/* mid content end */}
+
+            {/* lower content */}
+            <div className=" my-6">
+              <h1 className=" uppercase text-white text-lg font-bungee">tournament badges</h1>
+            </div>
+            <div className=" bg-[#282830] rounded-lg py-6  px-5">
+              <div className="grid grid-cols-6 gap-1">
+                <div className=" text-white flex col-start-1 col-end-6 ">
+                  <div>
+                    <img src={Cite} alt="/" />
+                  </div>
+                  <div className=" ml-4">
+                    <h1 className=" text-yellow-400 text-2xl mb-1 font-bungee">ABP CRIT BEAST</h1>
+                    <span>Yield Guild Games</span>
+                    <span className="ml-3">&bull; Highest MMR: 2.823</span>
+                    <span className="ml-3"><span className="text-white">&bull; </span>July 2021 - Sept 2021</span>
+                    <div>
+                      <p className=" pb-4 pt-4">Finished the tournament in 8th place among other SEA players.
+                  Tournament badge shown here.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className=" col-start-3 col-span-2 lg:col-start-6 lg:col-end-6">
+                  <img className=" w-full h-full" src={infinix} alt="/" />
+                </div>
+              </div>
+            </div>
+            {/* lower content end */}
+
+          </div>
+          {/* inner tab 3 ends */}
 
         </div>
         {/* tabs right side end */}
